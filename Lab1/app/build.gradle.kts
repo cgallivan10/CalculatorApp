af49a968cefd6b25de195e43cb8551d1f13b6496
Cgallivan10 <conorjgallivan@gmail.com>
2023-09-14 16:16:38 -0500

    Initial

--- Lab1/app/build.gradle.kts
+++ Lab1/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.cs407.helloworld"
+    namespace = "com.cs407.myapplication"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.cs407.helloworld"
+        applicationId = "com.cs407.myapplication"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -19,7 +19,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -31,7 +34,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- Lab1/settings.gradle.kts
+++ Lab1/settings.gradle.kts
@@ -13,5 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Hello World"
+rootProject.name = "My Application"
 include(":app")
+ 
